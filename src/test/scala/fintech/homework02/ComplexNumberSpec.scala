2a4a505b916d65cc0a1dbcb2db461ab95dad0096
Sapog123 <43165055+Sapog123@users.noreply.github.com>
2018-10-02 00:22:11 +0500

    Update ComplexNumberSpec.scala

--- src/test/scala/fintech/homework02/ComplexNumberSpec.scala
+++ src/test/scala/fintech/homework02/ComplexNumberSpec.scala
@@ -1,5 +1,4 @@
 package fintech.homework02
-package fintech.homework02
 import org.scalatest.{FlatSpec, Matchers}
 
 class ComplexNumberSpec extends FlatSpec with Matchers {
@@ -35,3 +34,4 @@ class ComplexNumberSpec extends FlatSpec with Matchers {
 
   }
 }
+
